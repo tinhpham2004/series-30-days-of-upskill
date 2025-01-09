293e2124cbae1f960ad1156b058988e0e43da7d2
tinhpham2004 <boydzpro3032004@gmail.com>
2025-01-09 23:21:03 +0700

    Refactor code style for consistency: adjust spacing and formatting in multiple files

--- Day1/bitwise-xor-of-all-pairings.cpp
+++ Day1/bitwise-xor-of-all-pairings.cpp
@@ -1,18 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+    int xorAllNums(vector<int> &nums1, vector<int> &nums2)
+    {
         long long res = 0;
-        if(nums1.size()%2 == 0 && nums2.size()%2 == 0) return 0;
-        if(nums1.size()%2 == 1){
-            for(int i=0;i<nums2.size();i++){
+        if (nums1.size() % 2 == 0 && nums2.size() % 2 == 0)
+            return 0;
+        if (nums1.size() % 2 == 1)
+        {
+            for (int i = 0; i < nums2.size(); i++)
+            {
                 res ^= nums2[i];
             }
         }
-        if(nums2.size()%2 == 1){
-            for(int i=0;i<nums1.size();i++){
+        if (nums2.size() % 2 == 1)
+        {
+            for (int i = 0; i < nums1.size(); i++)
+            {
                 res ^= nums1[i];
             }
         }
@@ -20,8 +27,8 @@ public:
     }
 };
 
-
-int main(){
+int main()
+{
     Solution sol;
     vector<int> nums1 = {2, 1, 3};
     vector<int> nums2 = {10, 2, 5, 0};
--- Day1/maximum-consecutive-floors-without-special-floors.cpp
+++ Day1/maximum-consecutive-floors-without-special-floors.cpp
@@ -1,17 +1,17 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution
 {
 public:
-    int maxConsecutive(int bottom, int top, vector<int>& special)
+    int maxConsecutive(int bottom, int top, vector<int> &special)
     {
         sort(special.begin(), special.end());
         int res = 0;
-        res = max(special[0]-bottom, top-special[special.size()-1]);
-        for(int i=1; i<special.size(); i++)
+        res = max(special[0] - bottom, top - special[special.size() - 1]);
+        for (int i = 1; i < special.size(); i++)
         {
-            res=max(res, special[i]-special[i-1]-1);
+            res = max(res, special[i] - special[i - 1] - 1);
         }
         return res;
     }
@@ -22,6 +22,6 @@ int main()
     Solution sol;
     int bottom = 28;
     int top = 50;
-    vector<int> special = {35,48};
-    cout<<sol.maxConsecutive(bottom, top, special);
+    vector<int> special = {35, 48};
+    cout << sol.maxConsecutive(bottom, top, special);
 }
